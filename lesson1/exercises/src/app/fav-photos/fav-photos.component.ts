2839911b109f9ef97e8353f96a1462df772ba7a8
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-01 00:59:32 -0500

    Started page title

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'This is a better section heading.';
+  image1 = 'https://a-z-animals.com/media/2020/01/Snake-Blue-viper-768x401.jpg';
+  image2 = 'https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg';
+  image3 = 'https://www.cbc.ca/kids/images/chinaanimals_header.jpg';
 
   constructor() { }
 
