3d7cc8f73b78e65f69c2dfcb842ee6d23766c260
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-01 02:03:43 -0500

    Finished exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'This is a better section heading.';
+  photosTitle = 'Pictures';
   image1 = 'https://a-z-animals.com/media/2020/01/Snake-Blue-viper-768x401.jpg';
   image2 = 'https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg';
   image3 = 'https://www.cbc.ca/kids/images/chinaanimals_header.jpg';
--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-title',
+  selector: 'page-title',
   templateUrl: './page-title.component.html',
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-
+title= 'Random stuff about me!'
   constructor() { }
 
   ngOnInit() {
