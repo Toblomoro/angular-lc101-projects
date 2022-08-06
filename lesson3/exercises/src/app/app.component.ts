d178746616f4f426a6c3b7efc79c517015e8acbd
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-06 00:43:29 -0500

    funcitons written

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title = "Exercises: Angular Lesson 3";
 
-  color = 'green';
+  color = "green";
   height = 0;
   width = 0;
-  message = 'Space shuttle ready for takeoff!';
+
+  flightStatus = "Space shuttle ready for takeoff!";
+  shuttleHeight = 0;
+
+  takeOff() {
+    confirm("Confirm that the shuttle is ready for takeoff.");
+    if (true) {
+      this.flightStatus = "Shuttle in flight";
+      this.shuttleHeight = 10000;
+      this.color = "blue";
+    }
+  }
+
+  land() {
+    alert("The shuttle is landing. Landing gear engaged.")
+    this.flightStatus = "The shuttle has landed.";
+      this.shuttleHeight = 0;
+      this.color = "green";
+  }
+  abortMission(){
+    confirm("Confirm you would like to abort the mission")
+    if (true) {
+      this.flightStatus = "Mission aborted.";
+      this.shuttleHeight = 0;
+      this.color = "red";
+    }
+  }
 }
--- lesson3/exercises/src/app/app.module.ts
+++ lesson3/exercises/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -13,4 +13,8 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
+
